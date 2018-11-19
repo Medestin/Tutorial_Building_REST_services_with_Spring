3b5e45d7da31d4725c302609659c2b8636ffe603
Medestin <30909893+Medestin@users.noreply.github.com>
2018-11-19 14:55:07 +0100

    feat: Simple Employee: Entity, Repo & CLR

---
+++ src/main/java/io/spring/RestServiceTutorial/payroll/Employee.java
@@ -0,0 +1,26 @@
+package io.spring.RestServiceTutorial.payroll;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+public class Employee {
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String role;
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
