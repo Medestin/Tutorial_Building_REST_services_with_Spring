515e2773ee3ebda9d1832935775f41d650957436
Medestin <30909893+Medestin@users.noreply.github.com>
2018-11-19 14:28:04 +0100

    Rest Service Tutorial - First commit

---
+++ src/main/java/io/spring/RestServiceTutorial/RestServiceTutorialApplication.java
@@ -0,0 +1,12 @@
+package io.spring.RestServiceTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceTutorialApplication.class, args);
+	}
+}
